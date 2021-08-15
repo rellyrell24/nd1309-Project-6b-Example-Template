bd1cbbd05a925dca572eeff570140cb7d03fb063
rellyrell24 <jg.techie24@gmail.com>
2021-08-14 22:34:24 -0500

    Implemented ConsumerRole

--- project-6/contracts/coffeeaccesscontrol/ConsumerRole.sol
+++ project-6/contracts/coffeeaccesscontrol/ConsumerRole.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.24;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.7;
 
 // Import the library 'Roles'
 import "./Roles.sol";
@@ -7,42 +8,48 @@ import "./Roles.sol";
 contract ConsumerRole {
 
   // Define 2 events, one for Adding, and other for Removing
+  event ConsumerAdded(address indexed account);
+  event ConsumerRemoved(address indexed account);
 
   // Define a struct 'consumers' by inheriting from 'Roles' library, struct Role
+  using Roles for Roles.Role;
+  Roles.Role private consumers;
 
   // In the constructor make the address that deploys this contract the 1st consumer
   constructor() public {
-    
+    _addConsumer(msg.sender);
   }
 
   // Define a modifier that checks to see if msg.sender has the appropriate role
   modifier onlyConsumer() {
-    
+    require(isConsumer(msg.sender), "Must be a consumer to call this functionality");
     _;
   }
 
   // Define a function 'isConsumer' to check this role
   function isConsumer(address account) public view returns (bool) {
-    
+    return consumers.has(account);
   }
 
   // Define a function 'addConsumer' that adds this role
   function addConsumer(address account) public onlyConsumer {
-    
+    _addConsumer(account);
   }
 
   // Define a function 'renounceConsumer' to renounce this role
   function renounceConsumer() public {
-    
+    _removeConsumer(msg.sender);
   }
 
   // Define an internal function '_addConsumer' to add this role, called by 'addConsumer'
   function _addConsumer(address account) internal {
-    
+    consumers.add(account);
+    emit ConsumerAdded(account);
   }
 
   // Define an internal function '_removeConsumer' to remove this role, called by 'removeConsumer'
   function _removeConsumer(address account) internal {
-    
+    consumers.remove(account);
+    emit ConsumerRemoved(account);
   }
 }
